648b8fbc1dd028719aa2942cc0051f1ff4b158d6
Iaroslav Makarchuk <iaroslavmakarchuk@Iaroslavs-MacBook-Pro.local>
2023-01-28 00:18:52 +0200

    Added Base ERC20 contract for log parsing

---
+++ BaseERC20.sol
@@ -0,0 +1,10 @@
+//Contract based on [https://docs.openzeppelin.com/contracts/4.x/erc721](https://docs.openzeppelin.com/contracts/4.x/erc721)
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BaseERC20 is ERC20, Ownable {
+    constructor() ERC20("", "") {}
+}
